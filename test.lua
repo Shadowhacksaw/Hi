26596e616f72b56aedf66364f672f298220be899
c00lhackkscriptservice <iezafira11611@gmail.com>
2025-05-24 18:26:29 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -52,7 +52,7 @@ local Window = Rayfield:CreateWindow({
 
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
 
-      Key = {"c00lhackkbackdoor"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"backstorysoon"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
 
    }
 
