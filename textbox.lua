0251f782e1dc847a982905ed76d3221c9f48728c
c00lhackkscriptservice <iezafira11611@gmail.com>
2025-05-25 18:19:11 +0800

    Update textbox.lua

--- textbox.lua
+++ textbox.lua
@@ -1,4 +1,30 @@
+local ScrnGui = Instance.new("ScreenGui")
+local MnPrt = Instance.new("Frame")
+local InPrt = Instance.new("Frame")
 local Cody = Instance.new("TextBox")
+
+ScrnGui.Name = "c00lhackkBackdoor"
+ScrnGui.Parent = game:GetService('CoreGui')
+ScrnGui.ResetOnSpawn = false
+
+MnPrt.Active = true
+MnPrt.BackgroundColor3 = Color3.new(0,0,0)
+MnPrt.BorderColor3 = Color3.new(1,0,0)
+MnPrt.Name = "MainPart"
+MnPrt.Parent = ScrnGui
+MnPrt.Position = UDim2.new(.5,-180,.5,-120)
+MnPrt.Size = UDim2.new(0,360,0,240)
+MnPrt.Draggable = true
+
+InPrt.Active = true
+InPrt.BackgroundColor3 = Color3.new(0,0,0)
+InPrt.BorderColor3 = Color3.new(1,0,0)
+InPrt.Name = "Inside"
+InPrt.Parent = MnPrt
+InPrt.Position = UDim2.new(0,0,0,50)
+InPrt.Size = UDim2.new(0,360,0,190)
+InPrt.Draggable = false
+
 
 Cody.Active = true
 Cody.BackgroundColor3 = Color3.new(0,0,0)
