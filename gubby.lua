e60fd0812a828e1d443906d452e088a41ff02f5e
c00lhackkscriptservice <iezafira11611@gmail.com>
2025-05-25 23:01:30 +0800

    Create gubby.lua

---
+++ gubby.lua
@@ -0,0 +1,24 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "hairy twinkles forsaken skin!",
+    Text = "Made by your brother",
+    Button1 = "Thank you:)",
+    Duration = 5
+})
+
+local args = {
+    "PurchaseContent",
+    game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Killers"):WaitForChild("Gubby")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Network"):WaitForChild("RemoteFunction"):InvokeServer(unpack(args))
+
+local args = {
+    "PurchaseContent",
+    game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Killers"):WaitForChild("FakePeppino")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Network"):WaitForChild("RemoteFunction"):InvokeServer(unpack(args))
+
+local args = {
+    "PurchaseContent",
+    game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Killers"):WaitForChild("Grug")
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Network"):WaitForChild("RemoteFunction"):InvokeServer(unpack(args))
