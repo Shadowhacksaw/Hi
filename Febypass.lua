59bd22782c6f907e02c8119889fa50134c893c89
c00lhackkscriptservice <iezafira11611@gmail.com>
2025-06-01 00:19:49 +0800

    Update Febypass.lua

--- Febypass.lua
+++ Febypass.lua
@@ -7,12 +7,18 @@ ScreenGui.Name = "FEBYPASS"
 local Frame = Instance.new("Frame")
 Frame.Parent = ScreenGui
 Frame.Size = UDim2.new(0, 400, 0, 100)
-Frame.Position = UDim2.new(0.5, -200, 0.5, -50 Message
+Frame.Position = UDim2.new(0.5, -200, 0.5, -50)
+Frame.BackgroundColor3 = Color3.new(0, 0, 0)
+Frame.BorderSizePixel = 0
+
+-- Add a TextLabel for the Message
 local TextLabel = Instance.new("TextLabel")
 TextLabel.Parent = Frame
 TextLabel.Size = UDim2.new(1, 0, 1, 0)
-TextLabel.Text = "FE Bypass activated!"
-TextLabel.TextColor3 = Color3.new(0, 1, 0)coroutine.wrap(function()
+TextLabel.Text = "FE Bypass Activated!"
+TextLabel.TextColor3 = Color3.new(0, 1, 0) -- Green text
+TextLabel.TextScaled = true
+TextLabel.Font =()
     for i = 1, 10 do
         Frame.BackgroundTransparency = i * 0.1
         wait(0.1)
@@ -20,9 +26,3 @@ TextLabel.TextColor3 = Color3.new(0, 1, 0)coroutine.wrap(function()
     wait(2)
     for i = 10, 1, -1 do
         Frame.BackgroundTransparency = i * 0.1
-        wait(0.1)
-    end
-    ScreenGui:Destroy()
-end)()
-
-print(" -- FE Bypass use before Patch!!")
