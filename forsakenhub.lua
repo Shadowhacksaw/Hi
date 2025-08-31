35444a433096f474df00ea3e9cc934aaa70bf2ee
c00lhackkscriptservice <iezafira11611@gmail.com>
2025-09-01 00:20:45 +0800

    Update forsakenhub.lua

--- forsakenhub.lua
+++ forsakenhub.lua
@@ -6,7 +6,7 @@ local Window = Rayfield:CreateWindow({
 
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
 
-   LoadingTitle = "c00lhackk script hub I pls wait",
+   LoadingTitle = "No more buying",
 
    LoadingSubtitle = "by Unknown dude",
 
