f47335cee6d689323ef29a882028ad24f4ed132a
c00lhackkscriptservice <iezafira11611@gmail.com>
2025-09-13 19:32:18 +0800

    Update tsb.lua

--- tsb.lua
+++ tsb.lua
@@ -11,8 +11,50 @@ local function playEmote(emoteName)
 game:GetService("Players").LocalPlayer.Character:WaitForChild("Communicate"):FireServer(unpack(args))
 end
 
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+
+   Name = "Hacksaken l Anti cheat bypasser",
+
+   Icon = 0,
+
+   LoadingTitle = "No more buying",
+   LoadingSubtitle = "by Unknown dude",
+
+   Theme = "DarkBlue",
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil,
+      FileName = "Emotes"
+   },
+
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
+   },
+
+   KeySystem = false,
+
+   KeySettings = {
+      Title = "c00hackk hub l key system",
+      Subtitle = "key is backstorysoon",
+      Note = "Key is backstorysoon",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"backstorysoon"}
+   }
+
+}) 
+
 --// Add Emote Tab
-local Tab2 = Window:CreateTab("Emotes", 130695581754590)
+local Tab1 = Window:CreateTab("Emotes", 130695581754590)
 
 -- Example emotes (you can expand this with the full list)
 local emoteNames = {
