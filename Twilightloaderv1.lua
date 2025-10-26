b3cafc39f5c43337ddeb1706d1c99c3b54ec1be6
c00lhackkscriptservice <iezafira11611@gmail.com>
2025-10-26 19:49:55 +0800

    Create Twilightloaderv1.lua

---
+++ Twilightloaderv1.lua
@@ -0,0 +1,43 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+
+   Name = "Twilight Area l i got nothing to say ",
+
+   Icon = 0,
+
+   LoadingTitle = "No more problems",
+   LoadingSubtitle = "by Unknown dude",
+
+   Theme = "DarkBlue",
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil,
+      FileName = "Twilight"
+   },
+
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
+   },
+
+   KeySystem = false,
+
+   KeySettings = {
+      Title = "c00hackk hub l key system",
+      Subtitle = "key is backstorysoon",
+      Note = "Key is backstorysoon",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"backstorysoon"}
+   }
+
+})
+
+local TabMain = 
